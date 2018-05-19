130eb53efd3ac4169b7fe825f67bcc788393de39
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-05-19 13:29:37 -0600

    replaces boilerplate welcome page with h1

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// list all books
+Route::get('api/books', 'BooksController@index');
+
+// list single book
+Route::get('api/book/{id}', 'BooksController@show');
+
+// create new book
+Route::post('api/book', 'BooksController@store');
+
+// update book
+Route::put('api/book', 'BooksController@store');
+
+// delete book 
+Route::delete('api/book/{id}', 'BooksController@destroy');
