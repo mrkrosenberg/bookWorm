1e27eb95c648b6a6f3354b52ce93c2d122bc1d55
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-05-19 15:11:27 -0600

    connects vue to backend, no actual front end yet

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,23 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        {{-- Protect against cross-site forgery in order to use Vue front end --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}" >
+        <script>window.Laravel = { csrfToken: '{{csrf_token()}}' }</script>
 
-        <title>Book-Worm</title>
+        <title>BookWorm</title>
 
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
     </head>
     <body>
-        <div>
-            <h1>Welcome to BookWorm</h1>
+        <div id="app">
+            <h1>Welcome to BookWorm, ya filthy animal</h1>
+            <div class="container">
+                <books></books>
+            </div>
         </div>
+
+    <script src="{{ asset('js/app.js') }}" ></script>
     </body>
 </html>
