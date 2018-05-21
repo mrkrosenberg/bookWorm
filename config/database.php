8ed888b459bcd1aaaa72be604a45926b56ebaa6f
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-05-21 10:42:17 -0600

    db region host added to database.php

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'US-East'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'heroku_e655a0d4d7d0627'),
             'username' => env('DB_USERNAME', 'b0f0210a970357'),
