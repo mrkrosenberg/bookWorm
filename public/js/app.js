346f738d463b1c14dd6c6b29798e8bc49e3e5d5e
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-05-21 10:06:20 -0600

    starts working on more responsive landing page

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,122 @@
-!function(n){var e={};function t(r){if(e[r])return e[r].exports;var o=e[r]={i:r,l:!1,exports:{}};return n[r].call(o.exports,o,o.exports,t),o.l=!0,o.exports}t.m=n,t.c=e,t.d=function(n,e,r){t.o(n,e)||Object.defineProperty(n,e,{configurable:!1,enumerable:!0,get:r})},t.n=function(n){var e=n&&n.__esModule?function(){return n.default}:function(){return n};return t.d(e,"a",e),e},t.o=function(n,e){return Object.prototype.hasOwnProperty.call(n,e)},t.p="/",t(t.s=0)}([function(n,e,t){t(1),n.exports=t(2)},function(n,e){var t=document.querySelector(".focus"),r="transparent 75px, rgba(0, 0, 0, 0.65) 200px)";function o(n){t.style.backgroundImage="radial-gradient(circle at "+n.pageX/window.innerWidth*100+"% "+n.pageY/window.innerHeight*100+"%, "+r}window.addEventListener("mousemove",function(n){o(n)}),window.addEventListener("mousedown",function(n){1===n.which&&(r="transparent 40px, rgba(0, 0, 0, 0.75) 50px)",o(n))}),window.addEventListener("mouseup",function(n){r="transparent 75px, rgba(0, 0, 0, 0.65) 200px)",o(n)})},function(n,e){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(1);
+module.exports = __webpack_require__(2);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+// require('./bootstrap');
+
+// window.Vue = require('vue');
+
+
+// Vue.component('books', require('./components/Books.vue'));
+// Vue.component('navbar', require('./components/Navbar.vue'));
+// Vue.component('book', require('./components/Book.vue'));
+
+// const app = new Vue({
+//     el: '#app'
+// });
+
+var focus = document.querySelector('.focus');
+
+var focusSize = 'transparent 75px, rgba(0, 0, 0, 0.65) 200px)';
+
+window.addEventListener("mousemove", function (e) {
+    updateFocus(e);
+});
+
+window.addEventListener('mousedown', function (e) {
+    if (e.which === 1) {
+        focusSize = 'transparent 40px, rgba(0, 0, 0, 0.75) 50px)';
+        updateFocus(e);
+    }
+});
+
+window.addEventListener("mouseup", function (e) {
+    focusSize = "transparent 75px, rgba(0, 0, 0, 0.65) 200px)";
+    updateFocus(e);
+});
+
+function updateFocus(e) {
+    focus.style.backgroundImage = 'radial-gradient(circle at ' + e.pageX / window.innerWidth * 100 + '% ' + e.pageY / window.innerHeight * 100 + '%, ' + focusSize;
+};
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+/******/ ]);
