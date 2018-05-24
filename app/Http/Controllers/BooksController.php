5e6579accc0902cdbed8c1384c9f5a70a19a4853
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-05-24 11:13:43 -0600

    removes unnecessary functions

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -27,16 +27,7 @@ class BooksController extends Controller
         return view('pages.books', compact('books'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        // call index function from here to re-render the page
-        
-    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -99,28 +90,6 @@ class BooksController extends Controller
 
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
