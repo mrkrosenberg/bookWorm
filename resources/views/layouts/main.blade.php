90a21f66b242f5f284e007b83a4a8cc6fd38cf61
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-05-25 12:52:48 -0600

    adds csrf token to main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
         {{-- Style sheets --}}
         <link rel="stylesheet" href="{{asset('css/app.css')}}" ></link>
         {{-- <link rel="stylesheet" href="{{asset('css/focus.css')}}"> --}}
