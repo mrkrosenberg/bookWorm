3cc47f4bb5905b5a8d14a2e8833c29b933f2c0e9
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-05-28 14:08:51 -0600

    updates book model to include relationship to user model

--- app/Book.php
+++ app/Book.php
@@ -4,5 +4,16 @@ namespace App;
 
 class Book extends Model
 {
-    //
+    // Table
+    protected $table = 'books';
+
+    // Primary Key
+    public $primary_key = 'id';
+
+    // Timestamps
+    public $timestamps = true;
+
+    public function user(){
+        return $this->belongsTo('App/User');
+    }
 }
