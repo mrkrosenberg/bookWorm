cdb937d2a890784f184e7e195ea5b2803cba304d
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-06-03 18:50:34 -0600

    adds more margin to header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-        <div class="header"> 
+        <div class="header mt-5"> 
             <h1 class="main-title">Welcome to Bookworm</h1>
             <a href="/index" class="btn--main btn--white btn--animated">build your reading list</a>
         </div>
