4600525bce1ef7fec03e69597bdecad064c26274
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-06-03 18:54:04 -0600

    makes header margin responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-        <div class="header mt-5"> 
+        <div class="header"> 
             <h1 class="main-title">Welcome to Bookworm</h1>
             <a href="/index" class="btn--main btn--white btn--animated">build your reading list</a>
         </div>
